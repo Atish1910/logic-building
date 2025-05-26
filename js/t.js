a0bf6519ef1172fd3ea1a87aa502f3e59770d242
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2025-05-26 13:19:54 +0530

    1

--- js/t.js
+++ js/t.js
@@ -1,1 +1,13 @@
-// 13 find a perfect Number : 28is perfect number 
+// String Reverse (Atish -> hsitA) => 187
+
+var str = "Atishhhhhhh";
+function strRev(iStr) {
+  reversed = "";
+  i = iStr.length - 1;
+  while (i >= 0) {
+    reversed = reversed + str[i];
+    i--;
+  }
+  console.log(reversed);
+}
+strRev(str);

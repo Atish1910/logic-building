72da29ba762e3ecbe351b1627301d8ffd2040b8a
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2025-06-13 12:57:50 +0530

    12

--- js/HandsOn/4ArrSort.js
+++ js/HandsOn/4ArrSort.js
@@ -1,11 +1,19 @@
 // Sort array
 
-a = [1, 4, 5, 62, 3, 4, 77, 1, 22, 3, 4, 5];
+Arr = [1, 4, 5, 62, 3, 4, 77, 1, 22, 3, 4, 5];
 
-function sortArray(a) {
-  iMin = a[i];
-  iMax = 
-  for (i = 0; i < a.length; i++) {
+function sortArray(Arr) {
+  for (i = 0; i < Arr.length; i++) {
+    let iMin = i;
+    for (j = i + 1; j < Arr.length; j++) {
+      if (Arr[j] < a[i]) {
+        iMin = j;
+      }
+    }
+    let temp = Arr[i];
+    Arr[i] = a[Min];
+    a[Min] = temp;
   }
+  console.log(Arr);
 }
-sortArray(a);
+sortArray(Arr);

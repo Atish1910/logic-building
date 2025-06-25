e47b8031d5a240dc1c5cd918a9f683b53e2afa59
Atish1910 <atishkamble398@gmail.com>
2025-06-25 21:07:54 +0530

    16

--- javaScript/t.js
+++ javaScript/t.js
@@ -1,21 +1,22 @@
-// MinimumDigit (IP 12345): (OP: 1) : 67
+// prime number from 1 to 100;
 
-iValue = 984727;
+var iValue = 100;
 
-function findMinimumDigit(iNo){
-  iDigit = 0;
-  iMin = 9;
-
-  while(iNo > 0){
-    iDigit = iNo % 10;
-    iNo = (iNo / 10) | 0;
-    // console.log(iDigit);
-
-    if(iMin > iDigit){
-      iDigit = iMin;
+function prime(iNo){
+    for(i = 2; i < iNo; i++){
+        
+        let iPrime = true;
+        
+        for(j = 0; j = i; j++){
+             if(i % j == 0){
+                iPrime = false;
+                break;
+             }
+        }
     }
-  }
-  console.log(iMin);
+    if(iPrime){
+        console.log(i)
+    }
 }
 
-findMinimumDigit(iValue);
+prime(iValue);

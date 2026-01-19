99b117e8c08ecacd9b9bfbff88e7b3b8a5eb8b8a
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2026-01-19 14:53:33 +0530

    push 19-2025

--- a.js
+++ a.js
@@ -1,36 +1,5 @@
-var payments = [
-  ["Swiggy", 123],
-  ["Swiggy", 227],
-  ["Zomato", 103],
-  ["Zomato", 171],
-  ["Dunzo", 131],
-  ["Zomato", 122],
-  ["Swiggy", 181],
-];
-
-function getNoSplurgeServices(data) {
-  var maxPaid = {};
-
-  // find max payment for each service
-  for (var i = 0; i < data.length; i++) {
-    var service = data[i][0];
-    var amount = data[i][1];
-
-    if (!maxPaid[service] || amount > maxPaid[service]) {
-      maxPaid[service] = amount;
-    }
-  }
-
-  // collect services with max < 200
-  var result = [];
-
-  for (var serviceName in maxPaid) {
-    if (maxPaid[serviceName] < 200) {
-      result.push(serviceName);
-    }
-  }
-
-  return result;
+for (var i = 1; i <= 3; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
 }
-
-console.log(getNoSplurgeServices(payments));
--- javaScript/15Promise.js
+++ javaScript/15Promise.js
@@ -1,28 +1,27 @@
-function fetchData(){
-    return new Promise((res, rej) => {
-        let success = false;
-        setTimeout(() => {
-            if(success){
-                res({
-                    name : "Atish",
-                    age : 26,
-                    city : "pune"
-                });
-            }else{
-                rej("Failed To Fetch Api details");
-            }
-
-        }, 1500)
-    });
+function fetchData() {
+  return new Promise((res, rej) => {
+    let success = false;
+    setTimeout(() => {
+      if (success) {
+        res({
+          name: "Atish",
+          age: 26,
+          city: "pune",
+        });
+      } else {
+        rej("Failed To Fetch Api details");
+      }
+    }, 1500);
+  });
 }
 
-async function getUser(){
-    try{
-        let user = await fetchData();
-        console.log("Api fetch Successful : ", user)
-    }catch(error){
-        console.log("Error : ", error)
-    }
+async function getUser() {
+  try {
+    let user = await fetchData();
+    console.log("Api fetch Successful : ", user);
+  } catch (error) {
+    console.log("Error : ", error);
+  }
 }
 
-getUser()
+getUser();

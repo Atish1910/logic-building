62336947111e514fc8dfabdc7209cfbedf6f1d78
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2026-04-15 18:37:34 +0530

    118

--- t.js
+++ t.js
@@ -1,15 +1,18 @@
-// 29 : flatten an array
-let iArr = [10,11,12,[12,45,67,8],[10]];
-function display(iArr){
-  let iResult = []
-  for(let i = 0; i < iArr.length; i++){
-    if(Array.isArray(iArr[i])){
-      iResult = iResult.concat(display(iArr[i]))
-    }else{
-      iResult.push(iArr[i]);
+const iArr = [10, 11, 121, 3, 14, 15, 6, 74];
+
+function Display(iArr) {
+  let iMax = -Infinity;
+  iMax2 = -Infinity;
+
+  for (let i = 0; i < iArr.length; i++) {
+    if (iArr[i] > iMax) {
+      iMax2 = iMax;
+      iMax = iArr[i];
+    } else if (iArr[i] > iMax2 && iArr[i] != iMax) {
+      iMax2 = iArr[i];
     }
   }
-  return iResult;
-};
+  console.log(iMax2);
+}
 
-console.log(display(iArr));
+Display(iArr);

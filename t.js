db4edec6e76d7a2535808fae78910dc7df670186
Atish1910 <atishkamble398@gmail.com>
2026-05-06 01:40:38 +0530

    125

--- t.js
+++ t.js
@@ -1,36 +1,8 @@
-<<<<<<< HEAD
-const iFlattenArr = [10,11,[12,23,15], [19]];
+// concat & remove duplicate
 
-function display(iArr){
-  iNewArr = [];
-  for (let i = 0; i < iArr.length; i++) {
-    if(Array.isArray(iArr[i])){
-      iNewArr.concat(iArr[i]);
-    }else{
-      iNewArr.push(iArr[i]);
-    }
-  }
-  console.log(iNewArr);
-}
+let iArr = ["atish kamble", "manthan shinde"];
 
-display(iFlattenArr)
-=======
-const iArr = [10, 11, 121, 3, 14, 15, 6, 74];
-
-function Display(iArr) {
-  let iMax = -Infinity;
-  iMax2 = -Infinity;
-
-  for (let i = 0; i < iArr.length; i++) {
-    if (iArr[i] > iMax) {
-      iMax2 = iMax;
-      iMax = iArr[i];
-    } else if (iArr[i] > iMax2 && iArr[i] != iMax) {
-      iMax2 = iArr[i];
-    }
-  }
-  console.log(iMax2);
-}
-
-Display(iArr);
->>>>>>> 62336947111e514fc8dfabdc7209cfbedf6f1d78
+let iResult = iArr.map((word) => {
+  return word[0].toUpperCase() + word.slice(1)
+});
+console.log(iResult);

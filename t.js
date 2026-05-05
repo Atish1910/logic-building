3a8e77f0825da6cb6a250e01d901b02ed1cca114
Atish1910 <atishkamble398@gmail.com>
2026-05-06 01:42:08 +0530

    126

--- t.js
+++ t.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const iFlattenArr = [10,11,[12,23,15], [19]];
 
 function display(iArr){
@@ -14,23 +13,3 @@ function display(iArr){
 }
 
 display(iFlattenArr)
-=======
-const iArr = [10, 11, 121, 3, 14, 15, 6, 74];
-
-function Display(iArr) {
-  let iMax = -Infinity;
-  iMax2 = -Infinity;
-
-  for (let i = 0; i < iArr.length; i++) {
-    if (iArr[i] > iMax) {
-      iMax2 = iMax;
-      iMax = iArr[i];
-    } else if (iArr[i] > iMax2 && iArr[i] != iMax) {
-      iMax2 = iArr[i];
-    }
-  }
-  console.log(iMax2);
-}
-
-Display(iArr);
->>>>>>> 62336947111e514fc8dfabdc7209cfbedf6f1d78

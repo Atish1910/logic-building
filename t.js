3158d9aac0eeb27e6e50082e7b37a0df671b640c
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2026-05-06 12:17:11 +0530

    128

--- t.js
+++ t.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const iFlattenArr = [10,11,[12,23,15], [19]];
-
-function display(iArr){
-  iNewArr = [];
-  for (let i = 0; i < iArr.length; i++) {
-    if(Array.isArray(iArr[i])){
-      iNewArr.concat(iArr[i]);
-    }else{
-      iNewArr.push(iArr[i]);
-    }
-  }
-  console.log(iNewArr);
-}
-
-display(iFlattenArr)
-=======
 const iArr = [10, 11, 121, 3, 14, 15, 6, 74];
 
 function Display(iArr) {
@@ -33,4 +16,3 @@ function Display(iArr) {
 }
 
 Display(iArr);
->>>>>>> 62336947111e514fc8dfabdc7209cfbedf6f1d78
